0e5be4b9949aaeb7d1e5f298a8622a8608d00245
Joan Molinas Ramon <joan@MacBookBPdeJoan.Home>
2015-12-10 10:29:49 +0100

    update zilean

--- Zilean/Zilean.swift
+++ Zilean/Zilean.swift
@@ -7,32 +7,28 @@ import Foundation
 
 class Zilean {
     
-    //MARK: Functions with Return
-    class func time <A> (
-        @autoclosure f: () -> A,
+    //MARK: Main functions
+    class func time <T> (
+        @autoclosure f f: () -> T,
         times : Int) -> Double {
             /*
             * Return :
             * Seconds to execute the function n times.
             */
-            print("/=====================================/")
-            print("Started")
             let start = NSDate()
-           
-            print("Executing function \(times) times")
+            
             for _ in 0..<times {
                 f()
             }
             
             let end = NSDate()
-            print("Finished")
             
             return end.timeIntervalSinceDate(start)
     }
     
-    class func time <A> (
-        @autoclosure f:() -> A,
-        @autoclosure f2:() -> A,
+    class func time <T> (
+        @autoclosure f f:() -> T,
+        @autoclosure f2:() -> T,
         times : Int) -> Double {
             /*
             * Return :
@@ -41,50 +37,113 @@ class Zilean {
             * -1 if function1 time was faster than function2 time
             */
             
-            let fTime = time(f, times : times)
-            let f2Time = time(f2, times : times)
+            let fTime = time(f: f, times: times)
+            let f2Time = time(f: f2, times : times)
             
             if fTime == f2Time { return 0 }
             else if fTime < f2Time { return -1 }
             else { return 1 }
     }
+}
+
+extension Zilean {
     
-    class func time <A> (
-        @autoclosure f:() -> A) -> Double {
-        
-            return time(f, times: 1)
+    //MARK: Most simple functions
+    class func time <T> (
+        @autoclosure f:() -> T) -> Double {
+            
+            return time(f: f, times: 1)
     }
     
-    class func time <A> (
-        @autoclosure f:() -> A,
-        @autoclosure f2:() -> A) -> Double {
-            return time(f, f2: f2, times: 1)
+    class func time <T> (
+        @autoclosure f f:() -> T,
+        @autoclosure f2:() -> T) -> Double {
+            return time(f: f, f2: f2, times: 1)
     }
-    
-    //MARK: Functions with Log
-    class func timeLog <A> (
-        @autoclosure f:() -> A) {
-            print("Seconds: \(time(f, times: 1))")
-    }
-    
-    class func timeLog <A> (
-        @autoclosure f:() -> A,
-        times : Int) {
-            print("Seconds: \(time(f, times: times))")
-    }
-    
-    class func timeLog <A> (
-        @autoclosure f:() -> A,
-        @autoclosure f2:() -> A)  {
-           print("Seconds: \(time(f, f2: f2, times: 1))")
-    }
-    
-    class func timeLog <A> (
-        @autoclosure f:() -> A,
-        @autoclosure f2:() -> A,
-        times : Int)  {
-            print("Seconds: \(time(f, f2: f2, times: times))")
-    }
-    
     
 }
+
+extension Zilean {
+    
+    //MARK: Functions with Log
+    class func timeLog <T> (
+        @autoclosure f f:() -> T) {
+            print("Seconds: \(time(f: f, times: 1))")
+    }
+    
+    class func timeLog <T> (
+        @autoclosure f f:() -> T,
+        times : Int) {
+            print("Seconds: \(time(f: f, times: times))")
+    }
+    
+    class func timeLog <T> (
+        @autoclosure f f:() -> T,
+        @autoclosure f2:() -> T)  {
+            print(time(f: f, f2: f2, times: 1))
+    }
+    
+    class func timeLog <T> (
+        @autoclosure f f:() -> T,
+        @autoclosure f2:() -> T,
+        times : Int)  {
+            print(time(f: f, f2: f2, times: times))
+    }
+}
+
+extension Zilean {
+    
+    //MARK: Functions return 2 value times
+    class func times <T> (
+        @autoclosure f f: () -> T,
+        @autoclosure f2 : () -> T,
+        times : Int) -> (Double, Double) {
+            let fTime = time(f: f, times: times)
+            let f2Time = time(f: f2, times : times)
+            
+            return (fTime, f2Time)
+            
+    }
+    
+    class func times <T> (
+        @autoclosure f f: () -> T,
+        @autoclosure f2 : () -> T) -> (Double, Double) {
+            let fTime = time(f: f, times: 1)
+            let f2Time = time(f: f2, times : 1)
+            
+            return (fTime, f2Time)
+            
+    }
+}
+
+/* EXPERIMENTALLY */
+
+extension Zilean {
+    
+    class func randomInt(range : ClosedInterval<Int>) -> Int {
+        return range.start + Int(arc4random_uniform(UInt32(range.end - range.start + 1)))
+    }
+    class func randomFloat(range: ClosedInterval<Float>) -> Float {
+        return range.start + (range.end - range.start) * Float(Float(arc4random()) / Float(UInt32.max))
+    }
+    
+    class func randomDouble(range: ClosedInterval<Double>) -> Double {
+        return range.start + (range.end - range.start) * Double(Double(arc4random()) / Double(UInt32.max))
+    }
+    
+    class func randomString(length : Int) -> String {
+        let characters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let mutableString = NSMutableString(capacity: length)
+        for var i = 0; i < length; i++ {
+            mutableString.appendFormat("%C", characters.characterAtIndex(Int(arc4random_uniform(UInt32(characters.length)))))
+        }
+        return mutableString as String
+    }
+}
+
+extension Zilean {
+    func timesWithRandomValues <T> (f : () -> T) {
+        
+    }
+}
+
